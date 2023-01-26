6b6cf85c4921462d0a0f4abd137e3c75d8e07587
Hoon <si932174@gmail.com>
2023-01-26 14:36:22 +0900

    feat : 입력 창 추가

--- App.js
+++ App.js
@@ -1,17 +1,59 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import theme from "./color";
+import CONSTANT from "./constant";
 
 export default function App() {
+  const [category, setCategory] = useState("work");
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.header}>
-        <TouchableOpacity>
-          <Text style={styles.btnText}>Work</Text>
+        <TouchableOpacity
+          onPress={() => {
+            setCategory("work");
+          }}
+        >
+          <Text
+            style={{
+              ...styles.btnText,
+              color: category === "work" ? "white" : theme.grey,
+            }}
+          >
+            Work
+          </Text>
         </TouchableOpacity>
-        <Text style={styles.btnText}>Life</Text>
+        <TouchableOpacity
+          onPress={() => {
+            setCategory("life");
+          }}
+        >
+          <Text
+            style={{
+              ...styles.btnText,
+              color: category === "life" ? "white" : theme.grey,
+            }}
+          >
+            Life
+          </Text>
+        </TouchableOpacity>
       </View>
+      <TextInput
+        placeholder={
+          category === "work"
+            ? CONSTANT.WORK_INPUT_MESSAGE
+            : CONSTANT.LIFE_INPUT_MESSAGE
+        }
+        style={styles.input}
+      />
     </View>
   );
 }
@@ -30,6 +72,13 @@ const styles = StyleSheet.create({
   btnText: {
     fontSize: 38,
     fontWeight: "600",
-    color: "white",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 20,
+    marginTop: 20,
+    fontSize: 16,
   },
 });
---
+++ constant.js
@@ -0,0 +1,6 @@
+const CONSTANT = {
+  WORK_INPUT_MESSAGE: "add to do for work",
+  LIFE_INPUT_MESSAGE: "add what you wanna do",
+};
+
+export default CONSTANT;
